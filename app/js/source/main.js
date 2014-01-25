d34824b10e52df21d0c1277739976c96d1e67840
Nat Webb <natrwebb@gmail.com>
2014-01-24 20:03:46 -0600

    1/25 almost done

--- app/js/source/main.js
+++ app/js/source/main.js
@@ -2,53 +2,116 @@
 
   'use strict';
 
+  var board = [[],[],[],[]];
+  var turn = 0;
+  var pickA;
+  var pickB;
+  var positionA = [];
+  var positionB = [];
+
   $(document).ready(initialize);
 
   function initialize(){
-    $('#play').click(clickPlay);
-    $('#shellHolder').on('click', '.shell', clickCheckAnswer);
-    resetCounter();
-  }
-
-  var winner = 0;
-
-  function resetCounter(){
-    $('#won').text('0');
-    $('#slash').text('/');
-    $('#played').text('0');
-    $('#percent').text('-');
+    $('#newGame').click(clickPlay);
+    $('#gameBoard td').click(showCard);
   }
 
   function clickPlay(){
-    var plays = parseInt($('#played').text()) + 1;
-    $('#played').text(plays);
-    winner = (Math.floor(Math.random()*3))+1;
-    for(var i = 1; i<4; i++){
-      var $newDiv = $('<div>');
-      $newDiv.text(i);
-      $newDiv.addClass('shell');
-      $('#shellHolder').append($newDiv);
+    $('.cardFront').each(function(){
+      $(this).text('');
+    });
+    var startingLetters = generateStartingLetters();
+    assignLettersToBoard(startingLetters);
+    $('#gameBoard').show();
+  }
+
+  function generateStartingLetters(){
+    var letters = 'abcdefghijklmnopqrstuvwxyz';
+    var chosenLetters = '';
+    for(var i = 0; i<10; i++){
+      var place = (Math.floor(Math.random()*(26-i)));
+      var pick = letters[place];
+      chosenLetters = chosenLetters + pick;
+      chosenLetters = chosenLetters + pick;
+      letters = letters.replace(pick, '');
+    }
+    return chosenLetters;
+  }
+
+  function assignLettersToBoard(letters){
+    var countdown = 0;
+
+    for(var i = 0; i<4; i++){
+      for(var j = 0; j<5; j++){
+        var place = (Math.floor(Math.random()*(20-countdown)));
+        var pick = letters[place];
+        board[i][j] = pick;
+        letters = letters.replace(pick, '');
+        countdown++;
+      }
     }
   }
 
-  function clickCheckAnswer(){
-    if(parseInt(this.textContent)===winner){
-      alert('You win!');
-      var score = parseInt($('#won').text()) + 1;
-      $('#won').text(score);
+  function showCard(){
+    if(positionA[0]===$(this).parent().index()&&positionA[1]===$(this).index()){
+      return;
+    }
+
+    if(turn===0){
+      positionA[0] = $(this).parent().index();
+      positionA[1] = $(this).index();
+      pickA = board[positionA[0]][positionA[1]];
+      $(this).children('.cardFront').text(pickA.toUpperCase());
+      $(this).children('.cardFront').css('opacity', '1');
+      $(this).children('.cardBack').css('opacity', '0');
+      turn = 1;
+    }
+    else if(turn===1){
+      positionB[0] = $(this).parent().index();
+      positionB[1] = $(this).index();
+      pickB = board[positionB[0]][positionB[1]];
+      $(this).children('.cardFront').text(pickB.toUpperCase());
+      $(this).children('.cardFront').css('opacity', '1');
+      $(this).children('.cardBack').css('opacity', '0');
+      checkCards();
+    }
+    else if(turn===2){
+      resetTurn();
+    }
+  }
+
+  function checkCards(){
+    positionA = [];
+    positionB = [];
+
+    if(pickA===pickB){
+      turn = 0;
+      if($('.cardFront[style="opacity: 1;"').size()===20){
+        alert('You win!');
+      }
     }
     else{
-      alert('Sorry, try again! The bean was in shell ' + winner + '!');
+      turn = 2;
     }
-    $('#shellHolder').empty();
-    $('#percent').text(calculatePercent() + '%');
   }
 
-  function calculatePercent(){
-    var percent = parseInt($('#won').text())/parseInt($('#played').text());
-    percent *= 100;
-    percent = Math.round(percent);
-    return percent;
+  function resetTurn(){
+    var row = positionA[0] + 1;
+    var column = positionA[1] + 1;
+    $('#gameBoard > tbody > tr:nth-child('+row+') > td:nth-child('+column+') .cardFront').css('opacity','0');
+    $('#gameBoard > tbody > tr:nth-child('+row+') > td:nth-child('+column+') .cardFront').text('');
+    $('#gameBoard > tbody > tr:nth-child('+row+') > td:nth-child('+column+') .cardBack').css('opacity','1');
+
+    row = positionB[0] + 1;
+    column = positionB[1] + 1;
+    $('#gameBoard > tbody > tr:nth-child('+row+') > td:nth-child('+column+') .cardFront').css('opacity','0');
+    $('#gameBoard > tbody > tr:nth-child('+row+') > td:nth-child('+column+') .cardFront').text('');
+    $('#gameBoard > tbody > tr:nth-child('+row+') > td:nth-child('+column+') .cardBack').css('opacity','1');
+
+    positionA = [];
+    positionB = [];
+
+    turn = 0;
   }
 
 })();
